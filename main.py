5732d909ccac63dd7957eee358f5888cf6e9f57a
Эмиль <gabdulvaleev.emil@mail.ru>
2021-03-03 22:45:49 +0300

    Registration form v2.0

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, url_for
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from flask import Flask, render_template, url_for, redirect
 import os
 from dotenv import load_dotenv
+from data import db_session
+from data.users import User
+from forms.user import RegisterForm
+from datetime import datetime
 
 
 app = Flask(__name__)
@@ -11,27 +12,37 @@ load_dotenv()
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
-class RegisterForm(FlaskForm):
-    astronaut_id = StringField('Login / email', validators=[DataRequired()])
-    astronaut_password = PasswordField('Password', validators=[DataRequired()])
-    commit_password = PasswordField('Repeat Password', validators=[DataRequired()])
-    surname = StringField('Surname', validators=[DataRequired()])
-    name = StringField('Name', validators=[DataRequired()])
-    age = StringField('Age', validators=[DataRequired()])
-    position = StringField('Position', validators=[DataRequired()])
-    speciality = StringField('Speciality', validators=[DataRequired()])
-    address = StringField('Address', validators=[DataRequired()])
-    submit_btn = SubmitField('Submit')
-
-
-@app.route('/register')
+@app.route('/register', methods=['POST', 'GET'])
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
-        pass  # Страницы редиректа пока нет
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            surname=form.surname.data,
+            name=form.name.data,
+            email=form.email.data,
+            created_date=datetime.now(),
+            age=form.age.data,
+            position=form.position.data,
+            speciality=form.speciality.data,
+            address=form.address.data,
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
     return render_template('register.html', title='Регистрация', base_css_directory=
                            url_for('static', filename='css/style.css'), form=form)
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    db_session.global_init('db/mars.db')
+    app.run(port=8000, host='127.0.0.1')
